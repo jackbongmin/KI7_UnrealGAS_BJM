0e9104ecc0b4056df3acc85e58f6f568c2a514f6
jackbongmin <rrrgk12@gmail.com>
2026-01-09 21:48:02 +0900

    0109_초기설정

---
+++ Source/KI7_UnrealGAS_BJM/KI7_UnrealGAS_BJM.Build.cs
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using UnrealBuildTool.Rules;
+
+public class KI7_UnrealGAS_BJM : ModuleRules
+{
+	public KI7_UnrealGAS_BJM(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Niagara", "EnhancedInput" });
+
+
+        PrivateDependencyModuleNames.AddRange(new string[] {
+        "GameplayAbilities", "GameplayTags", "GameplayTasks", "UMG", "AIModule", "NavigationSystem"
+        });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/KI7_UnrealGAS_BJM/KI7_UnrealGAS_BJM.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "KI7_UnrealGAS_BJM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KI7_UnrealGAS_BJM, "KI7_UnrealGAS_BJM" );
