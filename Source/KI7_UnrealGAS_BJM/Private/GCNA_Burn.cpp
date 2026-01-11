7f916c46a701b60f06718be407f70e6516a91165
jackbongmin <rrrgk12@gmail.com>
2026-01-11 17:49:10 +0900

    0111_패키징 버그 수정

--- Source/KI7_UnrealGAS_BJM/Private/GCNA_Burn.cpp
+++ Source/KI7_UnrealGAS_BJM/Private/GCNA_Burn.cpp
@@ -9,12 +9,14 @@
 
 AGCNA_Burn::AGCNA_Burn()
 {
-	GameplayCueTag = FGameplayTag::RequestGameplayTag(FName("GameplayCue.Debuff.Burn"));
-	bAutoDestroyOnRemove = true; 
+	//GameplayCueTag = FGameplayTag::RequestGameplayTag(FName("GameplayCue.Debuff.Burn"));
+	bAutoDestroyOnRemove = true;
+
 }
 
 bool AGCNA_Burn::OnActive_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters)
 {
+	UE_LOG(LogTemp, Warning, TEXT("[GCNA_Burn] OnActive 실행됨! 타겟: %s"), *GetNameSafe(MyTarget));
 	if (MyTarget && BurnVFX)
 	{
 		SpawnedLoopingEffect = UNiagaraFunctionLibrary::SpawnSystemAttached(
@@ -26,14 +28,24 @@ bool AGCNA_Burn::OnActive_Implementation(AActor* MyTarget, const FGameplayCuePar
 			EAttachLocation::SnapToTarget,
 			true
 		);
+		if (SpawnedLoopingEffect)
+		{
+			UE_LOG(LogTemp, Warning, TEXT(" [GCNA_Burn] 나이아가라 생성 성공!"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("[GCNA_Burn] 나이아가라 생성 실패! BurnVFX가 비어있나?"));
+		}
 
 		return true;
 	}
+	UE_LOG(LogTemp, Error, TEXT(" [GCNA_Burn] 타겟이 없거나 BurnVFX가 설정 안 됨!"));
 	return false;
 }
 
 bool AGCNA_Burn::OnRemove_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters)
 {
+	UE_LOG(LogTemp, Warning, TEXT("[GCNA_Burn] OnRemove 실행됨 (불 꺼짐)"));
 	if (SpawnedLoopingEffect)
 	{
 		SpawnedLoopingEffect->Deactivate();
